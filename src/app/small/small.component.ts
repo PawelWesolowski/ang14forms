4f294d8cab4641e458adef962227a58aaf2dea98
PawelWesolowski <pawesol@gmail.com>
2022-10-09 22:41:18 +0200

    form

--- src/app/small/small.component.ts
+++ src/app/small/small.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-small',
   templateUrl: './small.component.html',
   styleUrls: ['./small.component.scss']
 })
 export class SmallComponent implements OnInit {
+
+  twoDigit = new FormControl('');
 
   constructor() { }
 
