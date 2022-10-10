485a0d4daf5fd2c6c4072f3933f47c9dc3e2388c
Pawel Wesolowski <pawel.wesolowski@kantega.no>
2022-10-10 19:10:50 +0200

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ang14forms');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ang14forms app is running!');
-  });
 });
